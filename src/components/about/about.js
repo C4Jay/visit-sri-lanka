dea3fa3bb3b29485e584cebedc28db2aaca5015d
C4Jay <choolakejay@gmail.com>
2020-06-20 19:03:54 +0530

    button-style

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styles from './about.module.css';
+import {Link} from 'react-router-dom';
 
 class ImgPage extends Component {
     render () {
@@ -9,7 +10,7 @@ class ImgPage extends Component {
             <img src="https://images.unsplash.com/photo-1460627390041-532a28402358?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"></img>
             </div>
             <div className={styles.title}>
-            <h1>Visit Sri Lanka</h1>
+            <Link to="/"><h1>Visit Sri Lanka</h1></Link>
             </div>
             </div>
         )
